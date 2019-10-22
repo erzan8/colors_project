3e69d7a19f85d6d8fcec7ce21c4d88f2ce771cea
gcarron <guilhemcarron@gmail.com>
2019-10-22 13:52:15 +0200

    Added loader and second ambiance

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ const slider = document.querySelector('.slider');
 const logo = document.querySelector('#logo');
 const hamburger = document.querySelector('.hamburger');
 const headline = document.querySelector('.headline');
+const menu = document.querySelector('.menu');
+const main = document.querySelector('.main');
+const section1 = document.querySelector('.section1');
+const section2 = document.querySelector('.section2');
+const slider2 = document.querySelector('.slider2');
+const headline2 = document.querySelector('.headline2');
+const loader = document.querySelector('.loader');
 
 const tl = new TimelineMax();
 
-tl.fromTo(hero, 1, {height: "0%"}, {height:"80%", ease: Power2.easeInOut})
+onload = setTimeout("endLoader()", 8000);
+onload = setTimeout("Timeline()", 8000);
+
+function endLoader(){
+    loader.style.display = "none";
+    main.style.visibility = "visible";
+}
+
+function Timeline(){
+    tl.fromTo(hero, 1, {height: "0%"}, {height:"80%", ease: Power2.easeInOut})
     .fromTo(
         hero, 
         1.2, 
@@ -17,5 +33,37 @@ tl.fromTo(hero, 1, {height: "0%"}, {height:"80%", ease: Power2.easeInOut})
     .fromTo(logo, 0.5,{opacity: 0, x:30 }, { opacity: 1, x: 0}, 0.5)
     .fromTo(hamburger, 0.5,{opacity: 0, x:30 }, { opacity: 1, x: 0}, 1)
     .fromTo(headline, 1,{opacity: 0, marginTop:"100px"}, { opacity: 1, marginTop:"0"}, 2);
+}
 
 
+hamburger.addEventListener("click",() =>{
+    menu.classList.toggle('menu-active');
+})
+document.onkeydown = checkKey;
+
+let counter=0; 
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+
+    if (e.keyCode == '37') {
+       // left arrow
+       setTimeout("headline2.classList.remove('fade-up')", 100);
+       setTimeout("slider2.classList.remove('margin-slide')",300);
+       setTimeout("section2.classList.remove('transition-page2')",500);
+       setTimeout("section1.classList.remove('transition-page1')",700);
+       counter--;
+    
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+       section1.classList.add('transition-page1');
+       setTimeout("section2.classList.add('transition-page2')", 300);
+       setTimeout("slider2.classList.add('margin-slide')", 500);
+       setTimeout("headline2.classList.add('fade-up')", 800);
+       counter++;
+    }
+
+}
--- hovereffect.js
+++ hovereffect.js
@@ -1,7 +1,17 @@
-new hoverEffect({
+const effect1 = new hoverEffect({
     parent: document.querySelector('.distortion'),
     intensity:0.2,
     image1: './images/colors.jpg',
     image2:'./images/flowers.jpg',
-    displacementImage: './images/heightMap.png'
+    displacementImage: './images/heightMap.png',
+    imagesRatio: 0.7
+});
+
+const effect2 = new hoverEffect({
+    parent: document.querySelector('.distortion2'),
+    intensity:0.1,
+    image1: './images/neon1.jpg',
+    image2:'./images/neon2.jpg',
+    displacementImage: './images/stripe1.png',
+    imagesRatio: 0.7
 });
