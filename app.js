5ed93da10bc0d8da03f4635b25717e4b7b4d08f4
GuilhemC <48571176+erzan8@users.noreply.github.com>
2019-10-22 08:29:20 +0200

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.slider');
+const logo = document.querySelector('#logo');
+const hamburger = document.querySelector('.hamburger');
+const headline = document.querySelector('.headline');
+
+const tl = new TimelineMax();
+
+tl.fromTo(hero, 1, {height: "0%"}, {height:"80%", ease: Power2.easeInOut})
+    .fromTo(
+        hero, 
+        1.2, 
+        { transform:"scale(1.3)"}, 
+        { transform:"scale(1)", ease: Power2.easeInOut}
+        )
+    .fromTo(slider, 1.2, {x:"-100%"}, {x:"0%", ease: Power2.easeInOut}, 1.2)
+    .fromTo(logo, 0.5,{opacity: 0, x:30 }, { opacity: 1, x: 0}, 0.5)
+    .fromTo(hamburger, 0.5,{opacity: 0, x:30 }, { opacity: 1, x: 0}, 1)
+    .fromTo(headline, 1,{opacity: 0, marginTop:"100px"}, { opacity: 1, marginTop:"0"}, 2);
+
+
---
+++ hovereffect.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity:0.2,
+    image1: './images/colors.jpg',
+    image2:'./images/flowers.jpg',
+    displacementImage: './images/heightMap.png'
+});
